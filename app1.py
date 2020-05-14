d2c209d155eba63e43dfcfb6a396a8910717846d
Sergey Samoylov <ostatok@slans.com>
2020-05-15 00:47:39 +0700

    Version 1.2 (Titled words)

--- app1.py
+++ app1.py
@@ -7,6 +7,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes or N if no: " % get_close_matches(w, data.keys())[0])
         if yn.lower() == "y":
@@ -22,5 +24,8 @@ word = input("Enter a word: ")
 
 output = translate(word)
 
-for item in output:
-    print(item)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
